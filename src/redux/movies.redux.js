3ce6edde9a5cf6bd7cf465a111cf152af3953fcc
Principle <92553079+principlebrothers@users.noreply.github.com>
2022-08-28 13:43:21 +0000

    Add postlike, and getlike functions

--- src/redux/movies.redux.js
+++ src/redux/movies.redux.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const FETCH_MOVIES = 'movies-db-app/movies.redux.js/FETCH_MOVIES';
+const POST_LIKES = 'movies-db-app/movies.redux.js/POST_LIKES';
+const GET_LIKES = 'movies-db-app/movies.redux.js/GET_LIKES';
 const BASE_URL = 'https://yts.mx/api/v2/list_movies.json?genre=animation&limit=50&sort_by=download_count&minimum_rating=7';
+const LIKE_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/nWHbxSiuSFC7nMEf03JD/likes/';
 
 const initialState = [];
 
@@ -10,10 +13,33 @@ const moviesReducer = (state = initialState, action) => {
   switch (action.type) {
     case `${FETCH_MOVIES}/fulfilled`:
       return action.payload;
+    case `${POST_LIKES}/fulfilled`:
+      return state.map((movie) => {
+        if (movie.id === action.payload.id) {
+          return {
+            ...movie,
+            likes: movie.likes + 1,
+          };
+        } return movie;
+      });
+    case `${GET_LIKES}/fulfilled`:
+      return state.map((movie) => {
+        const movieLikes = action.payload.find((like) => like.item_id === movie.id);
+        if (movieLikes) {
+          return {
+            ...movie,
+            likes: movieLikes.likes,
+          };
+        } return {
+          ...movie,
+          likes: 0,
+        };
+      });
     default:
       return state;
   }
 };
+
 // restructured to use async thunk
 const restructredMovies = (data) => {
   const moviesContainer = [];
@@ -37,6 +63,19 @@ export const fetchMovies = createAsyncThunk(FETCH_MOVIES,
     const { data } = await axios.get(BASE_URL);
     return restructredMovies(data);
   });
+
+// Post Like to api
+export const postLike = createAsyncThunk(POST_LIKES, async (id) => {
+  const response = await axios.post(LIKE_URL, { item_id: id });
+  return response;
+});
+
+// get likes from api
+export const getLike = createAsyncThunk(GET_LIKES, async () => {
+  const response = await axios.get(LIKE_URL);
+  console.log(response.data.likes);
+  return response.data.likes;
+});
 
 export const selectAllMovies = (state) => state.movies;
 export default moviesReducer;
