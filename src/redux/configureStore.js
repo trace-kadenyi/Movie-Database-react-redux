24550faa2ea5b267d3d0c173a8f88245c4d4795b
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-08-28 22:28:56 +0300

    Fix linters errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,9 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import moviesReducer from './movies.redux';
-import likesReducer from './likes.redux';
 
 const rootReducer = combineReducers({
   movies: moviesReducer,
-  likes: likesReducer,
 });
 
 const store = configureStore({
