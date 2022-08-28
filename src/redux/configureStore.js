b6295ea66f337cb59646c56a51ae5abba0254a6c
Principle <92553079+principlebrothers@users.noreply.github.com>
2022-08-28 22:37:15 +0000

    Import the likeReducer from likes.redux.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import moviesReducer from './movies.redux';
+import likeReducer from './likes.redux';
 
 const rootReducer = combineReducers({
   movies: moviesReducer,
+  likes: likeReducer,
 });
 
 const store = configureStore({
