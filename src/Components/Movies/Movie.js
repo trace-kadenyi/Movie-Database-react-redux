3d65b2cac549fbab31c11ea963eb054e1db78523
kadenyi <treykadenyi@gmail.com>
2022-10-17 22:34:38 +0300

    Add usestate hook for the likeAdded state

--- src/Components/Movies/Movie.js
+++ src/Components/Movies/Movie.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { FaHeart } from 'react-icons/fa';
@@ -9,11 +9,15 @@ import './movies.css';
 const Movie = ({
   id, title, image, likes,
 }) => {
+  const [likeAdded, setLikeAdded] = useState(false);
   const dispatch = useDispatch();
 
   // Post like to api
-  const handleLike = () => {
+  const handleLike = (e) => {
+    if (likeAdded) return;
     dispatch(postLike(id));
+    setLikeAdded(true);
+    e.target.style.color = 'red';
   };
 
   return (
@@ -21,8 +25,8 @@ const Movie = ({
       <div className="mb-3 mainList">
         <img src={image} className="movie_image" alt={title} />
         <h3 className="movie_title my-2">{title}</h3>
-        { likes > 0 ? (<FaHeart className="heart liked" onClick={handleLike} />)
-          : (<FaHeart className="heart not_liked" onClick={handleLike} />)}
+        { likes > 0 ? (<FaHeart className="heart" onClick={handleLike} />)
+          : (<FaHeart className="heart" onClick={handleLike} />)}
         { likes === 1 ? (
           <span className="likes">
             {likes}
