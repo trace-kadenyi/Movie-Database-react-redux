f9f3ec26953194f1c60d4c406e479043f2db6cbe
kadenyi <treykadenyi@gmail.com>
2022-10-18 22:20:37 +0300

    Add footer path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Movies from './Components/Movies/Movies';
 import Header from './Components/Header/Header';
 import Comments from './Components/Comments/Comments';
+import Footer from './Components/Footer/Footer';
 
 const App = () => (
   <Router>
@@ -11,6 +12,7 @@ const App = () => (
       <Route path="/" element={<Movies />} />
       <Route path="comments/:id" element={<Comments />} />
     </Routes>
+    <Footer />
   </Router>
 );
 
